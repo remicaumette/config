b97a5c7df6c18e084824b02913b7058319c1bca1
Rémi Caumette <remicaumette@icloud.com>
2019-03-31 14:00:23 +0200

    custom theme for iterm

--- zsh.sh
+++ zsh.sh
@@ -6,5 +6,5 @@ cp ./zsh/zshrc ~/.zshrc
 ln -s ~/.zshrc ~/.config/zsh/.zshrc
 echo /usr/local/bin/zsh | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/zsh
-open ./iterm2/OneDark.itermcolors
+open ./iterm2/Custom.itermcolors
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
